6de44d9aa7e0c3c69e7ea7e0449a7596f899b922
이경원 <lkw1989@nhntoast.com>
2020-12-31 16:55:53 +0900

    SavePersonConfiguration 테스트 코드 작성

---
+++ src/test/java/fastcampus/spring/batch/part3/SavePersonConfigurationTest.java
@@ -0,0 +1,86 @@
+package fastcampus.spring.batch.part3;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBatchTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { SavePersonConfiguration.class, TestConfiguration.class})
+public class SavePersonConfigurationTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        personRepository.deleteAll();
+    }
+
+    @Test
+    public void test_not_allow_duplicate() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("allow_duplicate", "false")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        assertThat(jobExecution.getStepExecutions().stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum())
+                .isEqualTo(3);
+
+        assertThat(personRepository.count())
+                .isEqualTo(3L);
+    }
+
+    @Test
+    public void test_allow_duplicate() throws Exception {
+        // given
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("allow_duplicate", "true")
+                .toJobParameters();
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        assertThat(jobExecution.getStepExecutions().stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum())
+                .isEqualTo(100);
+
+        assertThat(personRepository.count())
+                .isEqualTo(100L);
+    }
+
+    @Test
+    public void test_step() {
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("savePersonStep");
+
+        assertThat(jobExecution.getStepExecutions().stream()
+                .mapToInt(StepExecution::getWriteCount)
+                .sum())
+                .isEqualTo(3);
+
+        assertThat(personRepository.count())
+                .isEqualTo(3L);
+    }
+}
